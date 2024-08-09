c91f1b3d24f1e4c43c4fbe17f62915077201581d
nebiyu youhannes <nebiyuyouhannes08@gmail.com>
2024-08-09 15:35:19 +0300

    fixed and ready for kaggle

--- main.py
+++ main.py
@@ -34,17 +34,15 @@ class WideDeep(nn.Module):
         self.month_embed=nn.Embedding(config.num_month,config.embedding_dim)
         self.time_day_embed=nn.Embedding(config.num_time_day,config.embedding_dim)
         self.year_embd=nn.Embedding(config.num_year,config.embedding_dim)
-        
-    
+           
         #deep
         self.deep=nn.Sequential(
-            nn.Linear(config.num_feature,1024), 
+            nn.Linear(600,1024), #config.num_feature
             nn.ReLU(),
             nn.Linear(1024,512),
             nn.ReLU(),
-            nn.Linear(512,2),  
+            nn.Linear(512,2)
         )
-
         self.config=config #this is used for testing
     
     def forward(self,product_id,user_id,year,month,day_of_week,hour):
@@ -55,8 +53,6 @@ class WideDeep(nn.Module):
 
         #the year is different
         #assert (year>=min_year).all() and  (year<=max_year).all(),"year is out of bound" 
-
-       
 
         assert (month>=0).all() and (month<self.config.num_month).all(),"month is out of bound"
 
@@ -81,17 +77,26 @@ class WideDeep(nn.Module):
         print(time_day_embed.shape)
 
         #feeding into wide
+        
+        #lets add more dims to the inputs
+        #product_id=product_id.unsqueeze(dim=1)
+        #user_id=user_id.unsqueeze(dim=1)
+
         wide_output=self.wide(torch.cat([product_id.float(),user_id.float()],dim=1))      #inputs are floats cause ouputs must be floats not torch long 
 
-        #feeding into deep
+        #feeding into deep but the shapes needs to be adjusted
         deep_input=torch.cat((
-                product_id_embed,
-                user_id_embed,
-                year_embed,
-                time_month_embed,
-                day_week_embed,
-                time_day_embed
+                product_id_embed.view(product_id_embed.size(0),-1),
+                user_id_embed.view(user_id_embed.size(0),-1),
+                year_embed.view(year_embed.size(0),-1),
+                time_month_embed.view(time_month_embed.size(0),-1),
+                day_week_embed.view(day_week_embed.size(0),-1),
+                time_day_embed.view(time_day_embed.size(0),-1)
         ),dim=1)
+
+        #output shape of the input to the deep
+        print("hello from model")
+        print(deep_input.shape) 
 
         #output of deep
         deep_output=self.deep(deep_input)   
